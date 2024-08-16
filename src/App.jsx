342a0caf22bdcb04b501e83db04522f5d5e21f44
Sudarshan Upadhaya <upadhayasudarshan29@gmail.com>
2024-08-16 15:37:46 +0545

    Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AdminForm from './Frontend/Form/AdminForm'
+
+
 import './App.css'
+import UserForm from './Frontend/Form/UserForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+      <AdminForm />
+      <UserForm/>
       
     </>
   )
