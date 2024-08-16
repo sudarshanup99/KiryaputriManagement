79266b8a9a1741b9ab1e20bb38a201a4c6d5c84b
shikshita23 <shikshitasubedi1@gmail.com>
2024-08-16 15:37:00 +0545

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Headers from './Frontend/Header/Headers'
 import './App.css'
+import Home from './Frontend/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Headers/>
+      <Home/>
     </>
   )
 }
