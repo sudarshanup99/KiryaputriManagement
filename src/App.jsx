206ebd32afaeaee1374b48bfc4249ac60f593d6c
Dikshya's PC <dikxu22@gmail.com>
2024-08-16 15:40:49 +0545

    updated details

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
+import Details from './Frontend/Details/Details'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Details />
       
     </>
   )
--- src/Frontend/Details/Details.jsx
+++ src/Frontend/Details/Details.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'antd';
+import house from "../../assets/house1.jpeg";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Details = () => {
-  return (
-    <div>
-      
+const { Meta } = Card;
+
+const notify = () => toast.success("Booking Successful");
+
+const Details = () => (
+  <div className="flex flex-col items-center justify-center min-h-screen">
+    <Card
+      style={{
+        width: 600,
+      }}
+      cover={
+        <img
+          alt="House"
+          src={house}
+        />
+      }
+    >
+      <Meta
+        title="Location: Nakhipot"
+      />
+      <div className="mt-4">
+        <ul className="list-disc ml-5">
+          <li>Status: Available</li>
+          <li>Service: Water, Washroom.......</li>
+        </ul>
+      </div>
+    </Card>
+    <div className="mt-4">
+      <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-sky-600" onClick={notify}>
+        Book Now
+      </button>
     </div>
-  )
-}
+    <ToastContainer />
+  </div>
+);
 
-export default Details
+export default Details;
